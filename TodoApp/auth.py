6e343ebab39c775cf2fca0be1410c1dc60c70072
ArslanRasheed60 <arslanrashee1@gmail.com>
2024-01-13 20:07:37 +0500

    hashing added

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from passlib.context import CryptContext
+from starlette import status
 
 router = APIRouter()
 
@@ -10,11 +11,17 @@ async def get_user():
 
 DB = []
 
+bcrypt_context = CryptContext(schemes=['bcrypt'])
+
 class User(BaseModel):
     username: str
     password: str
 
 
-@router.post("/auth")
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(user: User):
-    return user
+    DB.append({
+        'username': user.username,
+        'password': bcrypt_context.hash(user.password)
+    })
+    return DB
