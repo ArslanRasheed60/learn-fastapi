c9efa686d08e74f872f73721616db9da091496e0
ArslanRasheed60 <arslanrashee1@gmail.com>
2024-01-21 22:00:24 +0500

    .

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 from passlib.context import CryptContext
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -25,3 +26,7 @@ async def create_user(user: User):
         'password': bcrypt_context.hash(user.password)
     })
     return DB
+
+@router.post("/token", status_code=status.HTTP_201_CREATED)
+async def login_for_access_token():
+    pass
